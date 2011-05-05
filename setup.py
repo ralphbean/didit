002c9804a4fe39a08dac143971b26b215647655f
Ralph Bean <ralph.bean@gmail.com>
2011-05-05 11:29:05 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='didit',
+      version=version,
+      description="Commandline tool to remember what I did last week",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Ralph Bean',
+      author_email='ralph.bean@gmail.com',
+      url='http://github.com/ralphbean/didit',
+      license='GPLv3+',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
