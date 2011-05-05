6575730ca294421551f7e8c873c40b8478faa84a
Ralph Bean <ralph.bean@gmail.com>
2011-05-05 15:22:31 -0400

    This really has nothing to do with `operating systems`

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = f.read().strip()
 long_description = long_description.split('split here', 1)[1]
 f.close()
 
-version = '0.1.4'
+version = '0.1.5'
 
 setup(name='didit',
       version=version,
@@ -20,21 +20,6 @@ setup(name='didit',
           "Intended Audience :: System Administrators",
           "License :: OSI Approved :: GNU General Public License (GPL)",
           "Natural Language :: English",
-          "Operating System :: POSIX",
-          "Operating System :: POSIX :: AIX",
-          "Operating System :: POSIX :: BSD",
-          "Operating System :: POSIX :: BSD :: BSD/OS",
-          "Operating System :: POSIX :: BSD :: FreeBSD",
-          "Operating System :: POSIX :: BSD :: NetBSD",
-          "Operating System :: POSIX :: BSD :: OpenBSD",
-          "Operating System :: POSIX :: GNU Hurd",
-          "Operating System :: POSIX :: HP-UX",
-          "Operating System :: POSIX :: IRIX",
-          "Operating System :: POSIX :: Linux",
-          "Operating System :: POSIX :: Other",
-          "Operating System :: POSIX :: SCO",
-          "Operating System :: POSIX :: SunOS/Solaris",
-          "Operating System :: Unix",
           "Topic :: Office/Business :: News/Diary",
           "Topic :: Text Processing :: Markup",
           "Topic :: Utilities",
