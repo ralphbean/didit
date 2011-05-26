6d10881a3b3d3a02f6fbf7fd47eb0fafddac16b5
Ralph Bean <ralph.bean@gmail.com>
2011-05-26 15:29:42 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = f.read().strip()
 long_description = long_description.split('split here', 1)[1]
 f.close()
 
-version = '0.1.7'
+version = '0.2.0'
 
 setup(name='didit',
       version=version,
